99271205544a415f1df5537a4eaabefcda666697
Kuppuraj Muthu <mkuppuraj@zeomega.com>
2016-01-18 23:06:32 +0530

    Created form to make a fresh category

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rango.models import Category
+from rango.models import Page
+
+from rango.forms import CategoryForm
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            return form.errors
+    else:
+        form = CategoryForm()
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+def category(request, category_name_slug):
+    """
+    Gets pages for the given category slug name.
+    """
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'rango/category.html', context_dict)
 
 
 def index(request):
